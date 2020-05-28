c39ee7c780c2959042ef7d16fcf542071d1c3ea3
Tristan Carel <tristan.carel@gmail.com>
2020-05-28 08:54:58 +0200

    fixup! multi-comp-mesher: new OIST package / cgal: add 5.0.2

--- var/spack/repos/builtin/packages/multi-comp-mesher/package.py
+++ var/spack/repos/builtin/packages/multi-comp-mesher/package.py
@@ -10,11 +10,16 @@ class MultiCompMesher(CMakePackage):
     """Multi component mesh generator using CGAL"""
 
     homepage = "https://github.com/CNS-OIST/MultiCompMesher"
-    url = "git@github.com:CNS-OIST/MultiCompMesher.git"
+    url = "https://github.com/CNS-OIST/MultiCompMesher/archive/v1.0.0.tar.gz"
+    git = "git@github.com:CNS-OIST/MultiCompMesher.git"
 
     maintainers = ["WeiliangChenOIST", "tristan0x"]
 
-    version("develop", git=url)
+    version("develop", git=git)
+    version(
+        '1.0.0',
+        sha256='519e3aa4d390a6e6ed6ff1cba2a03e685191e45d00c5c06cf184c55a7c1340ab',
+    )
 
     variant(
         "mt",
